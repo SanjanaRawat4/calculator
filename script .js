0779af6cccb34ed7a06a7b807c2f00c0fdb77274
sana raw <sanjanarawat446@gmail.com>
2024-08-29 20:54:00 +0530

    files added

---
+++ script .js
@@ -0,0 +1,82 @@
+
+function seven(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("seven").innerHTML
+}
+function eight(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("eight").innerHTML
+}
+
+function nine(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("nine").innerHTML
+}
+
+function plus(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("plus").innerHTML
+}
+
+
+function four(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("four").innerHTML
+}
+
+function five(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("five").innerHTML
+}
+
+function six(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("six").innerHTML
+}
+
+function minus(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("minus").innerHTML
+}
+
+
+function one(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("one").innerHTML
+}
+
+function two(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("two").innerHTML
+}
+
+function three(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("three").innerHTML
+}
+
+function divide(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("divide").innerHTML
+}
+
+
+function zero(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("zero").innerHTML
+}
+
+function multiply(){
+                document.getElementById("txt").value= document.getElementById("txt").value+
+                document.getElementById("multiply").innerHTML
+}
+
+function resu(){
+                document.getElementById("txt").value= eval(document.getElementById("txt").value)
+}
+
+function reset(){
+                document.getElementById("txt").value= document.getElementById("")
+}
+
+
